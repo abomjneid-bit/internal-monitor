0b237a8f3b320d6288bb087a585453954492c626
abomjneid-bit <abomjneid@gmail.com>
2026-05-23 22:31:42 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -7,17 +8,33 @@ app.set("trust proxy", true);
 
 app.use(express.json());
 
+mongoose.connect(process.env.MONGO_URI);
+
+const VisitSchema = new mongoose.Schema({
+  ip: String,
+  time: String,
+  headers: Object,
+  data: Object
+});
+
+const Visit = mongoose.model("Visit", VisitSchema);
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.post("/log", (req, res) => {
+app.post("/log", async (req, res) => {
 
-  console.log("REAL IP:", req.ip);
+  const logData = {
+    ip: req.ip,
+    time: new Date().toISOString(),
+    headers: req.headers,
+    data: req.body
+  };
 
-  console.log("HEADERS:", req.headers);
+  console.log(logData);
 
-  console.log("DATA:", req.body);
+  await Visit.create(logData);
 
   res.sendStatus(200);
 });
